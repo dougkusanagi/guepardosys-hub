57d0a41cf9e61c1986341c0e2ef206f26a3c332c
Douglas Lopes <dl.aguiar@yahoo.com.br>
2023-06-11 21:31:28 -0300

    seeders organized

--- database/seeders/CompleteSeeder.php
+++ database/seeders/CompleteSeeder.php
@@ -4,13 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\Category;
-use App\Models\Company;
-use App\Models\Product;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class CompleteSeeder extends Seeder
 {
     use WithoutModelEvents;
 
