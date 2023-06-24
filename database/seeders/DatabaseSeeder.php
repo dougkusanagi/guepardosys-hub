813734e11c86ac1371a1f5ed1c6d87d8582405cb
Douglas Lopes <dl.aguiar@yahoo.com.br>
2023-06-24 16:45:51 -0300

    rename PartialSeeder as default DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class PartialSeeder extends Seeder
+class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
