5a4cf9c728931ac2f02a6fbbd75545795bf0200c
Douglas Lopes <dl.aguiar@yahoo.com.br>
2023-07-01 15:53:42 -0300

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,28 +17,34 @@ class ProductController extends Controller
 {
     public function index(): Response
     {
-        return inertia('Product/Index', [
-            'products' => Product::paginated(),
-            'product_status_array' => ProductStatusEnum::arrayFliped(),
-            'product_status_all' => ProductStatusEnum::array(),
-            'product_count_array' => ProductService::getStatusCountArray(),
-            'categories_all' => Category::query()
-                ->select('id', 'name')
-                ->whereBelongsTo(auth()->user()->company)
-                ->get(),
-            'per_page' => request('per_page', Product::perPage),
-        ]);
+        return inertia(
+            'Product/Index',
+            [
+                'products' => Product::paginated(),
+                'product_status_array' => ProductStatusEnum::arrayFliped(),
+                'product_status_all' => ProductStatusEnum::array(),
+                'product_count_array' => ProductService::getStatusCountArray(),
+                'categories_all' => Category::query()
+                    ->select('id', 'name')
+                    ->whereBelongsTo(auth()->user()->company)
+                    ->get(),
+                'per_page' => request('per_page', Product::perPage),
+            ]
+        );
     }
 
     public function create(): Response
     {
-        return inertia('Product/Create', [
-            'product_status_enum' => ProductStatusEnum::asSelectArray(),
-            'categories_all' => Category::query()
-                ->select('id', 'name')
-                ->whereBelongsTo(auth()->user()->company)
-                ->get(),
-        ]);
+        return inertia(
+            'Product/Create',
+            [
+                'product_status_enum' => ProductStatusEnum::asSelectArray(),
+                'categories_all' => Category::query()
+                    ->select('id', 'name')
+                    ->whereBelongsTo(auth()->user()->company)
+                    ->get(),
+            ]
+        );
     }
 
     public function store(StoreProductRequest $request): RedirectResponse
@@ -54,23 +60,27 @@ class ProductController extends Controller
     {
         $this->authorize('update', $product);
 
-        return inertia('Product/Edit', [
-            'product' => $product,
-            'product_status_enum' => ProductStatusEnum::asSelectArray(),
-            'categories_all' => Category::query()
-                ->select('id', 'name')
-                ->whereBelongsTo(auth()->user()->company)
-                ->get(),
-            'images' => $product->getMedia('images')
-                ->map(function (Media $media) {
-                    return ['id' => $media->id, 'url' => $media->getUrl()];
-                }),
-        ]);
+        return inertia(
+            'Product/Edit',
+            [
+                'product' => $product,
+                'product_status_enum' => ProductStatusEnum::asSelectArray(),
+                'categories_all' => Category::query()
+                    ->select('id', 'name')
+                    ->whereBelongsTo(auth()->user()->company)
+                    ->get(),
+                'images' => $product->getMedia('images')
+                    ->map(function (Media $media) {
+                        return ['id' => $media->id, 'url' => $media->getUrl()];
+                    }),
+            ]
+        );
     }
 
     public function update(UpdateProductRequest $request, Product $product): RedirectResponse
     {
         $this->authorize('update', $product);
+
         $product->update($request->validated());
         ProductImageService::registerCollections($request, $product);
 
@@ -81,6 +91,7 @@ class ProductController extends Controller
     public function destroy(Product $product): RedirectResponse
     {
         $this->authorize('delete', $product);
+
         $product->delete();
 
         return to_route('product.index')
